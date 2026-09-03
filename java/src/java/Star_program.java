c279ddc356bff09b3cff2016cf361d5cde5839d5
VISHAL S <VISHAL S@VISHAL>
2026-09-03 14:34:48 +0530

    star programs

---
+++ java/src/java/Star_program.java
@@ -0,0 +1,15 @@
+package java;
+
+public class Star_program {
+	public static void main(String[] args) 
+	{
+		for (int i=1; i<=5; i++)
+		{
+			for (int j=1; j<=i; j++);{
+				System.out.println("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
