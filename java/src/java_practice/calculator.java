f48576217b879b2b50fd5c91f4e59a41bcb1a8dc
VISHAL S <VISHAL S@VISHAL>
2026-09-09 10:35:08 +0530

    calculator

---
+++ java/src/java_practice/calculator.java
@@ -0,0 +1,19 @@
+package java_practice;
+
+public class calculator {
+	static int a =20;
+	static int b =5;
+	public static void main(String[] args) {
+	int sum=a+b;
+	System.out.println("Addition : "+sum);
+	int sub = a-b;
+	System.out.println("Subtraction : "+sub);
+	int mult = a*b;
+	System.out.println("Multiplication : "+mult);
+	int divi =a/b;
+	System.out.println("Division : "+divi);
+	int rema = a%b;
+	System.out.println("Remainder : "+rema);
+	
+	}
+}
